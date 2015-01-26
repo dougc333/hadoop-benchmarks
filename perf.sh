118fe8a27297c32e87dfc558e268254fa8be79d9
doug chang <dougc@stanfordalumni.org>
2015-01-26 09:47:14 -0800

    add

--- perf.sh
+++ perf.sh
@@ -245,9 +245,10 @@ function configblkfile {
   cd
   output=$(pwd)
   echo "pwd:$output" >> $LOGFILE
+  rm -rf tmpdir
   mkdir tmpdir
   cp /etc/sysconfig/dssd-blkdev /home/$USER/tmpdir/
-  sed -i -e 's/DSSD_BLKDEV_VOLUME_NAME=\"\"/DSSD_BLKDEV_VOLUME_NAME=\"\testhdfsvol\"/g' tmpdir/dssd-blkdev
+  sed -i -e 's/DSSD_BLKDEV_VOLUME_NAME=\"\"/DSSD_BLKDEV_VOLUME_NAME=\"/testhdfsvol\"/g' tmpdir/dssd-blkdev
   sed -i -e 's/DSSD_BLKDEV_LOGFILE_NAME=\"\"/DSSD_BLKDEV_LOGFILE_NAME=\"\/var\/log\/blkdev.log\"/g' tmpdir/dssd-blkdev
   sudo mv /etc/sysconfig/dssd-blkdev /etc/sysconfig/dssd-blkdevold
   sudo cp tmpdir/dssd-blkdev /etc/sysconfig/dssd-blkdev
