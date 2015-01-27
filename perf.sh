c115cc5f562d4b19f2bdc5da91e8c24097788838
doug chang <dougc@stanfordalumni.org>
2015-01-27 13:17:44 -0800

    add

--- perf.sh
+++ perf.sh
@@ -424,7 +424,7 @@ function modworkingdir {
   mkdir tmpsvc
   #modify working directory in hadooop-hdfs-namenode, hadoop-hdfs-datanode, hadoop-yarn-resourcemanager, hadoop-yarn-nodemanager
   cp /etc/init.d/hadoop-hdfs-namenode tmpsvc/hadoop-hdfs-namenode.orig
-  sudo sed 's/^WORKING_DIR.*/WORKING_DIR=\"$1\/lib\/hadoop-hdfs\"/' tmpsvc/hadoop-hdfs-namenode.orig > tmpsvc/savemenn
+  sudo sed 's/^WORKING_DIR.*/WORKING_DIR=\"{$1}\/lib\/hadoop-hdfs\"/' tmpsvc/hadoop-hdfs-namenode.orig > tmpsvc/savemenn
   sudo cp tmpsvc/savemenn /etc/init.d/hadoop-hdfs-namenode
 
 #  cp /etc/init.d/hadoop-hdfs-datanode tmpsvc/hadoop-hdfs-datanode.orig
