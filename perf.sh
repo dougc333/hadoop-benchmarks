2416e021b1ea1a666a7bce088a50760a3e4b2a2d
doug chang <dougc@stanfordalumni.org>
2015-01-28 16:26:24 -0800

    add

--- perf.sh
+++ perf.sh
@@ -533,7 +533,7 @@ function modsite {
   sudo chmod 0666 /etc/hadoop/conf/hdfs-site.xml
   cp /etc/hadoop/conf/hdfs-site.xml tmpsite/hdfs-site.xmlorig
   if [ -e tmpsite/hdfs-site.xmlorig ]; then
-    sed "s/var/testhdfsvol/g" tmpsite/hdfs-site.xmlorig > tmpsite/hdfs-site.xml
+    sed 's/var/'"$1"'/g' tmpsite/hdfs-site.xmlorig > tmpsite/hdfs-site.xml
   fi
   if [ -s tmpsite/hdfs-site.xml ] ;then
     sudo cp tmpsite/hdfs-site.xml /etc/hadoop/conf/
@@ -549,6 +549,17 @@ function modsite {
     sudo cp tmpsite/mapred-site.xml /etc/hadoop/conf/
   fi
   sudo chmod 0755 /etc/hadoop/conf/mapred-site.xml 
+
+  sudo chmod 0666 /etc/hadoop/conf/yarn-site.xml
+  cp /etc/hadoop/conf/yarn-site.xml tmpsite/yarn-site.xmlorig
+  if [ -e tmpsite/yarn-site.xmlorig ]; then 
+     sed 's/var/'"$1"'/g' tmpsite/yarn-site.xmlorig > tmpsite/yarn-site.xml
+  fi
+  if [ -s tmpsite/yarn-site.xml ]; then
+    sudo cp tmpsite/tmpsite/yarn-site.xml /etc/hadoop/conf/
+  fi
+  sudo chmod 0755 /etc/hadoop/conf/yarn-site.xml
+
 }
 
 function foo {
@@ -562,15 +573,6 @@ function foo {
   fi
   sudo chmod 0755 /etc/hadoop/conf/mapred-site.xml
 
-  sudo chmod 0666 /etc/hadoop/conf/yarn-site.xml
-  cp /etc/hadoop/conf/yarn-site.xml tmpsite/yarn-site.xmlorig
-  if [ -e tmpsite/yarn-site.xmlorig ]; then 
-     sed "s/var/\/$1/g" tmpsite/yarn-site.xmlorig > tmpsite/yarn-site.xml
-  fi
-  if [ -s tmpsite/yarn-site.xml ]; then
-    sudo cp tmpsite/tmpsite/yarn-site.xml /etc/hadoop/conf/
-  fi
-  sudo chmod 0755 /etc/hadoop/conf/yarn-site.xml
   
 }
 
