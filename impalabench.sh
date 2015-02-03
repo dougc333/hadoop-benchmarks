96a28f786c0f77182a671168a6d4dd9186eef878
doug chang <dougc@stanfordalumni.org>
2015-02-03 10:59:44 -0800

    add

---
+++ impalabench.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -p
+
+
+impala-shell -d tpcds_parquet -f q19.sql -o impalaoutput.txt
+
+cd ~/impala-tpcds-kit/queries
+for queryfile in $( ls *.sql ) do;
+  echo "processing $queryfile"
+  time impala-shell -d tpcds_parquet -f $queryfile -o impalaoutput$queryfile.txt
+end
+
+
