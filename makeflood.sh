8b9a9f625a03286be03e120cc54656d389337601
doug chang <dougc@stanfordalumni.org>
2015-02-11 16:32:09 -0800

    add

---
+++ makeflood.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -p
+
+
+
+opt/dssd/bin/flood create -V testhdfsvol1 -t block -F 4096 -l 4TB TestObj1
+opt/dssd/bin/flood create -V testhdfsvol2 -t block -F 4096 -l 4TB TestObj2
+opt/dssd/bin/flood create -V testhdfsvol3 -t block -F 4096 -l 4TB TestObj3
+opt/dssd/bin/flood create -V testhdfsvol4 -t block -F 4096 -l 4TB TestObj4
+opt/dssd/bin/flood create -V testhdfsvol5 -t block -F 4096 -l 4TB TestObj5
+opt/dssd/bin/flood create -V testhdfsvol6 -t block -F 4096 -l 4TB TestObj6
+opt/dssd/bin/flood create -V testhdfsvol7 -t block -F 4096 -l 4TB TestObj7
+opt/dssd/bin/flood create -V testhdfsvol8 -t block -F 4096 -l 4TB TestObj8
+opt/dssd/bin/flood create -V testhdfsvol9 -t block -F 4096 -l 4TB TestObj9
+opt/dssd/bin/flood create -V testhdfsvol10 -t block -F 4096 -l 4TB TestObj10
+opt/dssd/bin/flood create -V testhdfsvol11 -t block -F 4096 -l 4TB TestObj11
+opt/dssd/bin/flood create -V testhdfsvol12 -t block -F 4096 -l 4TB TestObj12
+opt/dssd/bin/flood create -V testhdfsvol13 -t block -F 4096 -l 4TB TestObj13
+opt/dssd/bin/flood create -V testhdfsvol14 -t block -F 4096 -l 4TB TestObj14
+opt/dssd/bin/flood create -V testhdfsvol15 -t block -F 4096 -l 4TB TestObj15
+opt/dssd/bin/flood create -V testhdfsvol16 -t block -F 4096 -l 4TB TestObj16
