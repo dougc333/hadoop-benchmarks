e1e85bc08da7be6bc04ac8e921d62183eb670367
doug chang <dougc@stanfordalumni.org>
2015-03-13 16:12:20 -0700

    add

---
+++ cmdaemons.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -p
+
+
+ssh root@r2491-d5-us04 "rpm -evv cloudera-manager-agent-5.3.2-1.cm532.p0.209.el6.x86_64"
+ssh root@r2491-d5-us05 "rpm -evv cloudera-manager-daemons-5.3.2-1.cm532.p0.209.el6.x86_64"
+ssh root@r2491-d5-us06 "rpm -evv cloudera-manager-agent-5.3.2-1.cm532.p0.209.el6.x86_64"
+ssh root@r2491-d5-us06 "rpm -evv cloudera-manager-daemons-5.3.2-1.cm532.p0.209.el6.x86_64"
+ssh root@r2491-d5-us07 "rpm -evv cloudera-manager-agent-5.3.2-1.cm532.p0.209.el6.x86_64"
+ssh root@r2491-d5-us07 "rpm -evv cloudera-manager-daemons-5.3.2-1.cm532.p0.209.el6.x86_64"
+ssh root@r2491-d5-us08 "rpm -evv cloudera-manager-agent-5.3.2-1.cm532.p0.209.el6.x86_64"
+ssh root@r2491-d5-us08 "rpm -evv cloudera-manager-daemons-5.3.2-1.cm532.p0.209.el6.x86_64"
+ssh root@r2491-d5-us09 "rpm -evv cloudera-manager-agent-5.3.2-1.cm532.p0.209.el6.x86_64"
+ssh root@r2491-d5-us09 "rpm -evv cloudera-manager-daemons-5.3.2-1.cm532.p0.209.el6.x86_64"
+ssh root@r2491-d5-us10 "rpm -evv cloudera-manager-agent-5.3.2-1.cm532.p0.209.el6.x86_64"
+ssh root@r2491-d5-us10 "rpm -evv cloudera-manager-daemons-5.3.2-1.cm532.p0.209.el6.x86_64"
